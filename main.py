2325d85d3d0007b3b0cb746b8ddae96969aae7c6
leyl1ne <e-khodin@inbox.ru>
2025-01-19 17:31:48 +0300

    created class Song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4    import BeautifulSoup
+import json
 
 
 HOST = "https://text-pesenok.ru/"
@@ -15,3 +16,33 @@ songs = soup.find_all("div", class_="item")
 
 for song in songs:
     print(song.find("a").get("href"))
+
+class Song:
+    def __init__(self, title="", artist="", lyrics="", album="", release_year=0, genre=""):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+        self.album = album
+        self.release_year = release_year
+        self.genre = genre 
+
+    def to_dict(self):
+        return{
+            'title' : self.title,
+            'artist' : self.artist,
+            'album' : self.album,
+            'release_year' : self.release_year,
+            'genre' : self.genre,
+            'lyrics' : self.lyrics
+        }
+    
+    def send_to_api(self, api_url):
+        data = self.to_dict()
+        headers = {'Content-Type': 'application/json'}
+
+        response = requests.post(api_url, data=json.dumps(data), headers=headers)
+
+        if response.status_code == 200:
+            print(f"Song '{self.title}' successfully send to API.")
+        else:
+            print(f"Failed to send song: {response.status_code} - {response.text}")
